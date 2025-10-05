7a2c983f8eb2c89e7cf54ff2165bf1d95d9a35d0
YK <molodoy@li.ru>
2025-10-05 15:09:41 +0300

    v 0.1.3.3

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // @name            WideView
 // @namespace       http://tampermonkey.net/
 // @license         MIT
-// @version         0.1.3.2
+// @version         0.1.3.3
 // @description     Removes sidebars + stretches post area to full browser width
 // @description:ru  Удаляет боковые панели + растягивает область поста на всю ширину браузера
 // @author          Prog57
@@ -13,6 +13,8 @@
 // @match           *://riptutorial.com/*
 // @match           *://bitbucket.org/*
 // @match           *://*.atlassian.net/*
+// @match           *://github.com/*
+// @match           *://pikabu.ru/*
 // @icon            https://www.google.com/s2/favicons?sz=64&domain=habr.com
 // @grant           none
 // @downloadURL     https://update.greasyfork.org/scripts/498363/WideView.user.js
@@ -22,7 +24,7 @@
 (function () {
     'use strict';
 
-    const apply = function (toWide, toHide, toMargin, percent) {
+    const apply = function (toWide, toHide, toMargin, toPadding, percent) {
         // debugger;
         let els = document.querySelectorAll(toHide) || [];
         // els.forEach(el => el.style.cssText += 'display: none;');
@@ -32,18 +34,28 @@
         els = document.querySelectorAll(toWide) || [];
         els.forEach(el => el.style.cssText += `max-width: ${percent}%; width: ${percent}%`);
 
-        document.querySelectorAll(toMargin)
-            .forEach(el => el.style.cssText += 'margin: 0;');
+        els = document.querySelectorAll(toMargin) || [];
+        // els.forEach < Element > (el => el.style.cssText += 'margin: 0, auto;'); не всегда работает
+        els.forEach(el => {
+            el.style.marginLeft = 'auto';
+            el.style.marginRight = 'auto';
+        });
+
+        document.querySelectorAll(toPadding)
+            .forEach(el => el.style.cssText += 'padding: 0;');
     }
 
     const run = function () {
         const host = window.location.host;
         if (/habr\.com/.test(host)) {
             apply(".tm-page-width, .tm-page__main_has-sidebar, .tm-article-presenter",
-                ".column_sidebar, .layout__navbar, .tm-page__sidebar");
+                ".column_sidebar, .layout__navbar, .tm-page__sidebar, .fixed-banner-wrapper",
+                null,
+                ".tm-page-width"
+            );
         }
         else if (/-linux\.net/.test(host)) {
-            apply("",
+            apply(null,
                 "#left_col");
         }
         else if (/microsoft\.com/.test(host)) {
@@ -65,6 +77,17 @@
         else if (/atlassian\.net/.test(host)) {
             apply(null,
                 "div.css-zolx62, link[href*='shared~vendor~atlassian'], script[data-defer-skip], .css-17lamoy");
+        }
+        else if (/github\.com/.test(host)) {
+            apply(".container-xl, .markdown-body",
+                null,
+                ".react-repos-overview-margin"
+            );
+        }
+        else if (/pikabu\.ru/.test(host)) {
+            apply(".app__inner, .main",
+                ".sidebar"
+            );
         }
     }
 
